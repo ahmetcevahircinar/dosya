b6c71a058589b24869ad6480710d656de8411798
Sentius <sentius@Sentius-MacBook-Pro.local>
2022-04-20 13:24:49 +0300

    ilk

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:dosya/views/albums_view.dart';
+import 'package:dosya/views/sozluk_view.dart';
+import 'package:flutter/material.dart';
+import 'file_download.dart';
+import 'file_operations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SozlukView(),
+    );
+  }
+}
---
+++ lib/services/albums_services.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../models/albums_model.dart';
+
+Future<List<Albums>> fetchData() async {
+  final response =
+  await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((data) => Albums.fromJson(data)).toList();
+  } else {
+    throw Exception('Unexpected error occured!');
+  }
+}
